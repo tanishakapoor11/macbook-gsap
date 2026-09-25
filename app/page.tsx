9aedc5e29b820a17c6b6a18bc5da53c88e925690
Tanisha Kapoor <tanisha@jupitermeta.io>
2026-09-25 15:59:58 +0530

    Implement hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Hero from "@/components/Hero";
 import NavBar from "@/components/NavBar";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <main>
       <NavBar />
+      <Hero />
     </main>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import React, { useEffect, useRef } from 'react'
+
+const Hero = () => {
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.playbackRate = 2;
+        }
+    }, [])
+  return (
+    <section id="hero">
+        <div>
+            <h1>MacBook Pro</h1>
+            <img src="/title.png" alt="MacBook Title" />
+        </div>
+
+        <video src='/videos/hero.mp4' ref={videoRef} autoPlay muted playsInline />
+        <button>Buy</button>
+        <p>From $1599 or $133/mo for 12 months</p>
+    </section>
+  )
+}
+
+export default Hero
