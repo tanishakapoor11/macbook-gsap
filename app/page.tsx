c849fbd0e04ee54cccd885a6745d8223a96c193e
Tanisha Kapoor <tanisha@jupitermeta.io>
2026-09-25 15:34:19 +0530

    Implement Apple nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import NavBar from "@/components/NavBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">App</h1>
-    </div>
+    <main>
+      <NavBar />
+    </main>
   );
 }
